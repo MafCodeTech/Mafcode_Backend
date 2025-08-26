7d304adf208eac7d0e0adb27e0132e5dff6cc8bc
Mohamed Nashaat <94199891+nashaat10@users.noreply.github.com>
2025-08-26 09:47:57 +0300

    update item model

--- src/models/itemModel.js
+++ src/models/itemModel.js
@@ -30,12 +30,7 @@ const itemSchema = new mongoose.Schema(
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
     },
-    status: {
-      type: String,
-      enum: ["lost", "found", "recovered"],
-      default: "lost",
-      required: [true, "Status is required"],
-    },
+
     active: {
       type: Boolean,
       default: true,
