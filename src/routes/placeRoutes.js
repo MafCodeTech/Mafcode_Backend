9670e95cc7a71b53e0ee3d4b75af1ee88decfefa
Mohamed Nashaat <94199891+nashaat10@users.noreply.github.com>
2025-08-27 14:38:26 +0300

    add and rm admins to places

--- src/routes/placeRoutes.js
+++ src/routes/placeRoutes.js
@@ -4,19 +4,27 @@ import * as authController from "../controllers/authController.js";
 
 const router = express.Router();
 
+router.use(authController.protect);
 router
   .route("/")
-  .post(
-    authController.protect,
-    authController.restrictTo("super Admin"),
-    placeController.addPlace
-  )
+  .post(authController.restrictTo("super Admin"), placeController.addPlace)
   .get(placeController.getPlaces);
+
+router
+  .route("/:id/admins")
+  .post(
+    authController.restrictTo("super-admin"),
+    placeController.assignAdminsToPlace
+  )
+  .delete(
+    authController.restrictTo("super-admin"),
+    placeController.removeAdminFromPlace
+  );
 
 router
   .route("/:id")
   .get(placeController.getPlaceById)
-  .patch(authController.protect, placeController.updatePlace)
-  .delete(authController.protect, placeController.deletePlace);
+  .patch(placeController.updatePlace)
+  .delete(placeController.deletePlace);
 
 export default router;
